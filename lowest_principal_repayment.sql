7f4caefabe95b82213f19d92d5ab5991d9912887
Gabriel Vitor S. S. <124630375+GabrielVitorSS@users.noreply.github.com>
2025-04-24 03:24:53 -0300

    Create lowest_principal_repayment.sql

---
+++ lowest_principal_repayment.sql
@@ -0,0 +1,6 @@
+SELECT country_name, indicator_name, MIN(debt) AS lowest_repayment
+FROM international_debt
+WHERE indicator_code = 'DT.AMT.DLXF.CD'
+GROUP BY country_name, indicator_name
+ORDER BY lowest_repayment
+LIMIT 1;
